3fc1729ba74c043fd49ba7d11edba76f31b71215
Siddharth Srivastava <sidmkp96@gmail.com>
2014-08-29 16:32:33 +0530

    Initial comit

---
+++ swiftTimerAppBeta/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  swiftTimerAppBeta
+//
+//  Created by Siddharth Srivastava on 29/08/14.
+//  Copyright (c) 2014 Siddharth Srivastava. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+                            
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
---
+++ swiftTimerAppBetaTests/swiftTimerAppBetaTests.swift
@@ -0,0 +1,36 @@
+//
+//  swiftTimerAppBetaTests.swift
+//  swiftTimerAppBetaTests
+//
+//  Created by Siddharth Srivastava on 29/08/14.
+//  Copyright (c) 2014 Siddharth Srivastava. All rights reserved.
+//
+
+import UIKit
+import XCTest
+
+class swiftTimerAppBetaTests: XCTestCase {
+    
+    override func setUp() {
+        super.setUp()
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+    
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+        super.tearDown()
+    }
+    
+    func testExample() {
+        // This is an example of a functional test case.
+        XCTAssert(true, "Pass")
+    }
+    
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measureBlock() {
+            // Put the code you want to measure the time of here.
+        }
+    }
+    
+}
